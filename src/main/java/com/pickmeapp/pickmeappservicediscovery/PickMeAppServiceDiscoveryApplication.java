17fd9207cb7e06c9b0fba62987dba991218fb957
Rajat Pathak <rajatpathak9888@gmail.com>
2025-03-29 01:37:17 +0530

    adding Eureak server dependency

---
+++ src/main/java/com/pickmeapp/pickmeappservicediscovery/PickMeAppServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.pickmeapp.pickmeappservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PickMeAppServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickMeAppServiceDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pickmeapp/pickmeappservicediscovery/PickMeAppServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickmeapp.pickmeappservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickMeAppServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
